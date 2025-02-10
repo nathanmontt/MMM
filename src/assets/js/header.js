c39c56144e84ec6e802fecd33aaf5bbcb9a0a151
nathanmontt <contato.mont@gmail.com>
2025-02-10 18:53:38 -0300

    Testes

---
+++ src/assets/js/header.js
@@ -0,0 +1,28 @@
+// 'use strict'
+
+// const buttonMenu = document.querySelector('.menu-btn-cta');
+// const optionsLinks = document.querySelectorAll('.options-holder');
+
+// buttonMenu.addEventListener("click", function () {
+//     console.log("teste");
+//     optionsLinks.forEach(element => {
+//         if (element.classList.contains('hidden')) {
+//             element.classList.remove('hidden');
+//         }
+//     })
+// })
+// if (hambMenuBtn && menuItems.length > 0) {
+//     hambMenuBtn.addEventListener('click', function () {
+//         menuItems.forEach(element => {
+//             if (element.classList.contains('hidden')) {
+//                 element.classList.remove('hidden');
+//                 hambMenuBtn.textContent = 'FECHAR';
+//                 menuItemsHolder.classList.add('pg-1');
+//             } else {
+//                 element.classList.add('hidden');
+//                 hambMenuBtn.textContent = 'MENU';
+//                 menuItemsHolder.classList.remove('pg-1');
+//             }
+//         });
+//     });
+// }
