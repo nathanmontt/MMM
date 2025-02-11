90a666f21c203983dd5f9f22f5ccde8f0ff79b13
nathanmontt <contato.mont@gmail.com>
2025-02-11 15:42:58 -0300

    Testes no JS

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,4 +1,44 @@
-// 'use strict'
+'use strict'
+
+// Primeiro, começamos criando o evento do scroll. Usamos window (eu acho que é pra isso) quando não sabemos qual elemento usar
+
+window.addEventListener('scroll', function () {
+    const valuePx = 670;
+    const changeColorMenuOptions = document.querySelectorAll('.options-holder');
+    /* Criamos uma var que armazena o valor do scrollPosY. Como assim?
+     * scrollPosY armazena o valor atual dos pixels presentes na tela. Ou seja, para trocarmos as cores das opções do menu, precisamos saber qual posição vamos fazer essa troca
+    */
+
+    let xValue = 1;
+
+    const holderOfPosYValue = window.scrollY;
+    if (holderOfPosYValue > valuePx) {
+        document.body.style.backgroundColor = "red";
+    } else {
+        this.document.body.style.backgroundColor = "transparent"
+    }
+
+    // if (holderOfPosYValue > valuePx) {
+    //     changeColorMenuOptions.forEach (function (e) {
+    //         e.style.backgroundColor = "blue"
+    //     })
+    // } else {
+    //     console.log(xValue);
+    // }
+
+    // changeColorMenuOptions.forEach(function (el) {
+    //     if (holderOfPosYValue > valuePx) {
+    //         el.style.backgroundColor = "red";
+    //     }
+    // });
+    // changeColorMenuOptions.forEach(element => {
+    //     if (holderOfPosYValue > valuePx) {
+    //         element.style.color = "red";
+    //     } else {
+    //         console.log("ainda n");
+    //     }
+    // })
+});
 
 // const buttonMenu = document.querySelector('.menu-btn-cta');
 // const optionsLinks = document.querySelectorAll('.options-holder');
