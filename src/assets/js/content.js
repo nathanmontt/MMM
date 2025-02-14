6043f65167c215eb2227104671c047e34a01cb35
nathanmontt <contato.mont@gmail.com>
2025-02-14 15:55:39 -0300

    Testes no JS

--- src/assets/js/content.js
+++ src/assets/js/content.js
@@ -3,12 +3,14 @@
 // Criação de objeto com infos sobre os estudos diários
 
 /* Universidade */
-const days = [
+const university = [
     {
-        date: '2023-09-01',
+        date: 'DD/MM/AAAA',
         subject: 'Redes',
         study: [
             `Estudei redes`,
+            `Estudei redes2 `,
+            `Estudei redes   4`,
         ]
     },
 ];
@@ -16,44 +18,85 @@ const days = [
 /* Carreira */
 const carrer = [
     {
-        date: '2023-09-01',
-        push: '1° Push',
+        date: 'DD/MM/AAAA',
+        gitPushCount: '1° Push',
         study: [
             `Fiz isso, isso e isso`,
         ]
     },
 ];
 
+/* Hobby */
 const hobby = [
     {
-        date: '2023-09-01',
-        subject: 'Beatmaking/Games',
+        date: 'DD/MM/AAAA',
         study: [
             `Fiz isso, isso, isso e mais isso`,
         ]
     },
 ];
 
-/* Função que cria as divs */
-function renderDay(day) {
-    // Cria um container para a Uni, Carreira e Hobby
+/* Função que cria as tags */
+function renderUniDay(day) {
+    /* Cria um container para a Uni, Carreira e Hobby.
+    Dentro desse container, é aonde vai entrar o conteúdo que queremos mostrar*/
     const uniContainer = document.createElement('div');
-    uniContainer.classList.add('day-block');
+
+    /* Criamos e adicionamos classes às divs de cima */
+    uniContainer.classList.add('uni-day-block');
   
-    // Cria um título com a data
-    const dayTitle = document.createElement('h2');
-    dayTitle.textContent = `Dia: ${day.date}`;
-    uniContainer.appendChild(dayTitle);
+    // Criam-se tags que armazenam algumas infos. inciais
+    const textMainTitle = document.createElement('h2');
+    const subjectOfTheDay = document.createElement('p');
+
+    textMainTitle.textContent = `Dia ${day.date}`;
+    subjectOfTheDay.textContent = `${day.subject}`;
+    
+    uniContainer.appendChild(textMainTitle);
+    uniContainer.appendChild(subjectOfTheDay);
+    // textMainTitle.style.fontFamily = 'Cormorant Garamond, serif';
   
-    // Cria a lista <ul> para as atividades
-    const ul = document.createElement('ul');
+    // Cria a tag <ul> para as atividades
+    const ulHolderItemStudy = document.createElement('ul');
     day.study.forEach(activity => {
-        const li = document.createElement('li');
-        li.textContent = activity;
-        ul.appendChild(li);
+        const liItemStudy = document.createElement('li');
+        liItemStudy.textContent = activity;
+        ulHolderItemStudy.appendChild(liItemStudy);
     });
   
-    uniContainer.appendChild(ul);
+    uniContainer.appendChild(ulHolderItemStudy);
+  
+    return uniContainer;
+}
+
+function renderCarrearDay(day) {
+    /* Cria um container para a Uni, Carreira e Hobby.
+    Dentro desse container, é aonde vai entrar o conteúdo que queremos mostrar*/
+    const carrerContainer = document.createElement('div');
+
+    /* Criamos e adicionamos classes às divs de cima */
+    carrerContainer.classList.add('carrer-day-block');
+  
+    // Criam-se tags que armazenam algumas infos. inciais
+    const textMainTitle = document.createElement('h2');
+    const gitPushOfTheDay = document.createElement('p');
+
+    textMainTitle.textContent = `Dia ${day.date}`;
+    subjectOfTheDay.textContent = `${day.subject}`;
+    
+    uniContainer.appendChild(textMainTitle);
+    uniContainer.appendChild(subjectOfTheDay);
+    // textMainTitle.style.fontFamily = 'Cormorant Garamond, serif';
+  
+    // Cria a tag <ul> para as atividades
+    const ulHolderItemStudy = document.createElement('ul');
+    day.study.forEach(activity => {
+        const liItemStudy = document.createElement('li');
+        liItemStudy.textContent = activity;
+        ulHolderItemStudy.appendChild(liItemStudy);
+    });
+  
+    uniContainer.appendChild(ulHolderItemStudy);
   
     return uniContainer;
 }
@@ -66,8 +109,8 @@ function renderAllDays() {
     container.innerHTML = '';
   
     // Percorre o array 'days' e renderiza cada dia
-    days.forEach(day => {
-        const dayElement = renderDay(day);
+    university.forEach(day => {
+        const dayElement = renderUniDay(day);
         container.appendChild(dayElement);
     });
 }
