b87da8c808a6908c0f54d21ebbf830f5baec4468
nathanmontt <contato.mont@gmail.com>
2025-02-13 23:28:37 -0300

    Testes e testes

--- src/assets/js/content.js
+++ src/assets/js/content.js
@@ -6,34 +6,56 @@
 const days = [
     {
         date: '2023-09-01',
-        activities: [
-            'Estudei 2h de Lógica de Programação',
-            'Li 10 páginas de um livro sobre Carreira de Web Dev'
+        subject: 'Redes',
+        study: [
+            `Estudei redes`,
         ]
     },
 ];
 
+/* Carreira */
+const carrer = [
+    {
+        date: '2023-09-01',
+        push: '1° Push',
+        study: [
+            `Fiz isso, isso e isso`,
+        ]
+    },
+];
+
+const hobby = [
+    {
+        date: '2023-09-01',
+        subject: 'Beatmaking/Games',
+        study: [
+            `Fiz isso, isso, isso e mais isso`,
+        ]
+    },
+];
+
+/* Função que cria as divs */
 function renderDay(day) {
-    // Cria um container para o dia
-    const dayContainer = document.createElement('div');
-    dayContainer.classList.add('day-block');
+    // Cria um container para a Uni, Carreira e Hobby
+    const uniContainer = document.createElement('div');
+    uniContainer.classList.add('day-block');
   
     // Cria um título com a data
     const dayTitle = document.createElement('h2');
     dayTitle.textContent = `Dia: ${day.date}`;
-    dayContainer.appendChild(dayTitle);
+    uniContainer.appendChild(dayTitle);
   
     // Cria a lista <ul> para as atividades
     const ul = document.createElement('ul');
-    day.activities.forEach(activity => {
+    day.study.forEach(activity => {
         const li = document.createElement('li');
         li.textContent = activity;
         ul.appendChild(li);
     });
   
-    dayContainer.appendChild(ul);
+    uniContainer.appendChild(ul);
   
-    return dayContainer;
+    return uniContainer;
 }
 
 // Função responsável por adicionar os containers dentro do container principal
