cfff81bb51931f0a3965d68b419555bf1bd927f8
nathanmontt <contato.mont@gmail.com>
2025-02-16 21:18:10 -0300

    Testes e finalização da parte simples
    Partindo, antes de mais nada, para a estilização pelo JS

--- src/assets/js/hover.js
+++ src/assets/js/hover.js
@@ -1,26 +1,12 @@
 'use strict'
 
-// Variáveis para a animação do hover em "mais sobre"
-const linkSeeMore = document.querySelector('.view-more-link');
-const rightArrow  = document.getElementById('icon-see-more');
-let varEmValSeeMore = '0.55em';
-let transformValTime = '0.3s';
-
 // Variáveis para a animação do hover em "explorar"
 const textExplore = document.querySelector('.explore');
 const downArrow = document.getElementById('icon');
+let transformValTime = '0.3s';
 let varEmValEx = '0.8em';
 
 /* -- FUNÇÕES -- */
-linkSeeMore.addEventListener('mouseover', function handleMouseOverSeeMore () {
-    rightArrow.style.transform = `translateX(${varEmValSeeMore})`;
-    rightArrow.style.transition = `transform ${transformValTime}`;
-});
-
-linkSeeMore.addEventListener('mouseout', function handleMouseOutSeeMore() {
-    rightArrow.style.transform = `translateX(0)`;
-});
-
 textExplore.addEventListener('mouseover', function handleMouseOverExplore () {
     downArrow.style.transform = `translateY(${varEmValEx})`;
     downArrow.style.transition = `transform ${transformValTime}`;
