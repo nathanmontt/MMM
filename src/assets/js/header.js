182c0e2e2cba782c09dc40b2d4f78caa19b622c1
nathanmontt <contato.mont@gmail.com>
2025-03-06 15:46:26 -0300

    Adição (3)
    Adição do esqueleto em CSS do menu e da hero sections

--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,42 +1,42 @@
-'use strict'
+// 'use strict'
 
-// Função para modificar a cor das fontes quando a tela descer até um determinado ponto
-window.addEventListener('scroll', function changeFontMenuOptionsColor () {
-    // Variável local da função para armazenar o valor do scroll;
-    let varVal = 670;
+// // Função para modificar a cor das fontes quando a tela descer até um determinado ponto
+// window.addEventListener('scroll', function changeFontMenuOptionsColor () {
+//     // Variável local da função para armazenar o valor do scroll;
+//     let varVal = 670;
     
-    // Declaração dos elementos que serão usados no processo;
-    const logoProject = document.querySelector('.logo');
-    const iconDropDown = document.getElementById('icon');
-    const changeColorMenuOptions = document.querySelectorAll('.links');
+//     // Declaração dos elementos que serão usados no processo;
+//     const logoProject = document.querySelector('.logo');
+//     const iconDropDown = document.getElementById('icon');
+//     const changeColorMenuOptions = document.querySelectorAll('.links');
     
-    // Na hora de fazer a verificação, eu poderia colocar o window.scrollY, porém é mais simples armazená-lo em uma variável;
-    // Lembrando que o scrollY é do tipo number, ou seja, ele nos retorna um valor;
-    const holderOfPosYValue = window.scrollY;
+//     // Na hora de fazer a verificação, eu poderia colocar o window.scrollY, porém é mais simples armazená-lo em uma variável;
+//     // Lembrando que o scrollY é do tipo number, ou seja, ele nos retorna um valor;
+//     const holderOfPosYValue = window.scrollY;
     
-    // Usamos esse valor para verificar com a varVal que definimos anteriormente;
-    // Logo do projeto
-    if (holderOfPosYValue > varVal) {
-        logoProject.style.color = "black";
-    } else {
-        logoProject.style.color = "white";
-    }
+//     // Usamos esse valor para verificar com a varVal que definimos anteriormente;
+//     // Logo do projeto
+//     if (holderOfPosYValue > varVal) {
+//         logoProject.style.color = "black";
+//     } else {
+//         logoProject.style.color = "white";
+//     }
     
-    // Como não é apenas um elemento, sempre usar o método forEach()
-    // Cada elemento do menu
-    changeColorMenuOptions.forEach(el => {
-        if (holderOfPosYValue > varVal) {
-            el.style.color = "black";
-        } else {
-            el.style.color = "white";
-        }
-    });
+//     // Como não é apenas um elemento, sempre usar o método forEach()
+//     // Cada elemento do menu
+//     changeColorMenuOptions.forEach(el => {
+//         if (holderOfPosYValue > varVal) {
+//             el.style.color = "black";
+//         } else {
+//             el.style.color = "white";
+//         }
+//     });
 
-    // Achado no W3School, se tornou uma praticidade mudar os atributos ao invés de modificar um elemento (imagem, gif, etc) como um todo. Não sabia. Conhecimento novo
-    // Ícone do menu (dropdown)
-    if (holderOfPosYValue > varVal) {
-        iconDropDown.setAttribute("fill", "#000000");
-    } else {
-        iconDropDown.setAttribute("fill", "#FFFFFF");
-    }
-});
+//     // Achado no W3School, se tornou uma praticidade mudar os atributos ao invés de modificar um elemento (imagem, gif, etc) como um todo. Não sabia. Conhecimento novo
+//     // Ícone do menu (dropdown)
+//     if (holderOfPosYValue > varVal) {
+//         iconDropDown.setAttribute("fill", "#000000");
+//     } else {
+//         iconDropDown.setAttribute("fill", "#FFFFFF");
+//     }
+// });
--- src/assets/js/hover.js
+++ src/assets/js/hover.js
@@ -1,17 +1,17 @@
-'use strict'
+// 'use strict'
 
-// Variáveis para a animação do hover em "explorar"
-const textExplore = document.querySelector('.explore');
-const downArrow = document.getElementById('icon');
-let transformValTime = '0.3s';
-let varEmValEx = '0.8em';
+// // Variáveis para a animação do hover em "explorar"
+// const textExplore = document.querySelector('.explore');
+// const downArrow = document.getElementById('icon');
+// let transformValTime = '0.3s';
+// let varEmValEx = '0.8em';
 
-/* -- FUNÇÕES -- */
-textExplore.addEventListener('mouseover', function handleMouseOverExplore () {
-    downArrow.style.transform = `translateY(${varEmValEx})`;
-    downArrow.style.transition = `transform ${transformValTime}`;
-});
+// /* -- FUNÇÕES -- */
+// textExplore.addEventListener('mouseover', function handleMouseOverExplore () {
+//     downArrow.style.transform = `translateY(${varEmValEx})`;
+//     downArrow.style.transition = `transform ${transformValTime}`;
+// });
 
-textExplore.addEventListener('mouseout', function handleMouseOutExplore () {
-    downArrow.style.transform = `translateY(0.5em)`;
-});
+// textExplore.addEventListener('mouseout', function handleMouseOutExplore () {
+//     downArrow.style.transform = `translateY(0.5em)`;
+// });
