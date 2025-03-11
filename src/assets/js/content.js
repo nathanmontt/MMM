50a37b661c57f48fd8b3ba4859f14b68590d7960
nathanmontt <contato.mont@gmail.com>
2025-03-11 18:28:49 -0300

    Testes (1)
    Tentando pensar na lógica da modal para os conteúdos, além de testes de debuug

--- src/assets/js/content.js
+++ src/assets/js/content.js
@@ -1,44 +1,16 @@
-// 'use strict'
+'use strict'
 
-// const subtopicListCareer = document.querySelector(".subtopic-list-career");
-// const subtopicListUni = document.querySelector(".subtopic-list-uni");
-// const subtopicListHobby = document.querySelector(".subtopic-list-hobby");
+const modalLinks = document.getElementsByClassName("open-modal");
+let k;
 
-// const topicOne = document.querySelector(".btn-cta-career");
-// const topicTwo = document.querySelector(".btn-cta-uni");
-// const topicThree = document.querySelector(".btn-cta-hobby");
-
-// // Changing the "+" for "-"
-// const one = document.querySelector(".one");
-// const two = document.querySelector(".two");
-// const three = document.querySelector(".three");
-
-// topicOne.addEventListener("click", function openFirstTopic () {
-//     if (subtopicListCareer.classList.contains("hidden")) {
-//         subtopicListCareer.classList.remove("hidden");
-//         one.textContent = '-'
-//     } else {
-//         subtopicListCareer.classList.add("hidden");
-//         one.textContent = '+'
-//     }
-// });
-
-// topicTwo.addEventListener("click", function openSecondTopic () {
-//     if (subtopicListUni.classList.contains("hidden")) {
-//         subtopicListUni.classList.remove("hidden");
-//         two.textContent = '-'
-//     } else {
-//         subtopicListUni.classList.add("hidden");
-//         two.textContent = '+'
-//     }
-// });
-
-// topicThree.addEventListener("click", function openThirdTopic () {
-//     if (subtopicListHobby.classList.contains("hidden")) {
-//         subtopicListHobby.classList.remove("hidden");
-//         three.textContent = '-'
-//     } else {
-//         subtopicListHobby.classList.add("hidden");
-//         three.textContent = '+'
-//     }
-// });
+for (k=0 ; k< modalLinks.length ; k++) {
+    modalLinks[k].addEventListener("click", function () {
+        const containerContent = this.nextElementSibling;
+        
+        if (containerContent.classList.contains("hidden")) {
+            containerContent.classList.remove("hidden");
+        } else {
+            containerContent.classList.add("hidden");
+        }
+    });
+}
