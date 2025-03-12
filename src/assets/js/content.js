4488abe6f1eddbfcf4e8b125cfdfea0e5d0895c1
nathanmontt <contato.mont@gmail.com>
2025-03-12 19:49:33 -0300

    Testes (4)
    Ainda na lógica da modal

--- src/assets/js/content.js
+++ src/assets/js/content.js
@@ -1,16 +1,18 @@
 'use strict'
 
 const modalLinks = document.getElementsByClassName("open-modal");
+const contentOne = document.querySelector(".container-frontend");
+const contentTwo = document.querySelector(".container-backend");
 let k;
 
 for (k=0 ; k< modalLinks.length ; k++) {
     modalLinks[k].addEventListener("click", function () {
-        const containerContent = this.nextElementSibling;
-        
-        if (containerContent.classList.contains("hidden")) {
-            containerContent.classList.remove("hidden");
+        if (contentOne.classList.contains("hidden") || contentTwo.classList.contains("hid")) {
+            contentOne.classList.remove("hidden");
+            contentTwo.classList.remove("hid");
         } else {
-            containerContent.classList.add("hidden");
+            contentOne.classList.add("hidden");
+            contentTwo.classList.add("hid");
         }
     });
 }
