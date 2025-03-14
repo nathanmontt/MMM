f683932b4ecb17e44c3c7fdde7f3db242e139d85
nathanmontt <contato.mont@gmail.com>
2025-03-14 17:27:33 -0300

    Entendimento da lógica do problema quase 100%

--- src/assets/js/content.js
+++ src/assets/js/content.js
@@ -2,38 +2,19 @@
 
 const modalLinks = document.getElementsByClassName("open-modal");
 const container = document.querySelectorAll(".container");
-const contentOne = document.querySelector(".container-frontend");
-const contentTwo = document.querySelector(".container-backend");
+let frontend = document.querySelector(".content-type-frontend");
 
 // Uma das opções viáveis é varrer todo o conteudo em modalLinks e depois verificar um por um e ver onde cada um entra
 
-for (let k=0 ; k< modalLinks.length ; k++) {
-    modalLinks[k].addEventListener("click", function () {
-        console.log("debuggs 1");
-        container.forEach(el => {
-            // if (el.style.display === "none") {
-            //     console.log("testeqwe");
-            // } else {
-            //     console.log("teste");
-            // }
-            el.classList.toggle("hidden");
-        })
-        // let container = this.nextElementSibling;
-        // if (contentOne.classList.contains("hidden") || contentTwo.classList.contains("hid")) {
-        //     contentOne.classList.remove("hidden");
-        //     contentTwo.classList.remove("hid");
-        // } else {
-        //     contentOne.classList.add("hidden");
-        //     contentTwo.classList.add("hid");
-        // }
+// for (let k=0 ; k< modalLinks.length ; k++) {
+//     modalLinks[k].addEventListener("click", function () {
+//         console.log("debuggs 1");
 
-        // if (container.style.display === "none") {
-        //     console.log("testeqwe");
-        // } else { 
-        //     console.log("teste");
-        // }
 
-        // contentOne.classList.toggle("hidden");
-        // contentTwo.classList.toggle("hid");
-    });
-}
+//         if (frontend.classList.contains('hidden')) {
+//             frontend.classList.remove("hidden");
+//         } else {
+//             frontend.classList.add("hidden");
+//         }
+//     });
+// }
